396924a584597d6011ec1a1c4af7e090a694ec26
NetAcad DEVASC <changeme@example.com>
2024-06-18 21:27:51 +0000

    devnet2.py

---
+++ devnet2.py
@@ -0,0 +1,10 @@
+# Solicitar datos al usuario
+nombre = input("Por favor, ingresa tu nombre: ")
+apellido = input("Ahora ingresa tu apellido: ")
+edad = input("Finalmente, ingresa tu edad: ")
+
+# Imprimir los datos ingresados por el usuario
+print("Datos ingresados:")
+print("Nombre:", nombre)
+print("Apellido:", apellido)
+print("Edad:", edad)
